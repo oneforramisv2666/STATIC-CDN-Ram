eb27a62bf02b4d20594c45c0aff9351f9e7a724a
Ram Song <88095693+RamSong@users.noreply.github.com>
2022-06-04 20:05:55 +0800

    Create blog-main-tongji.js

---
+++ blog/js/blog-main-tongji.js
@@ -0,0 +1,17 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?d89f00031d0c29fac5e18d356b5cceab";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+<!-- Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "b74831b19eeb4b208efb7b0f9565b481"}'></script><!-- End Cloudflare Web Analytics -->
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "c722nodt17");
+</script>
